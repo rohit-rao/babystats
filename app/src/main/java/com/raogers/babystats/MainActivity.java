cab2ab38e801f121cd01823e7276a24584d008bd
Rohit Rao <rohit-rao@users.noreply.github.com>
2017-10-15 13:46:38 -0400

    Creates empty recycler views for the three main activities.

--- app/src/main/java/com/raogers/babystats/MainActivity.java
+++ app/src/main/java/com/raogers/babystats/MainActivity.java
@@ -1,7 +1,9 @@
 package com.raogers.babystats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnDiapersButtonClicked(View view) {
+        // TODO(rohitrao): Restructure as pushing a new fragment onto the stack.
+        Intent intent = new Intent(this, DiapersListActivity.class);
+        startActivity(intent);
+    }
+
+    public void OnFoodButtonClicked(View view) {
+        Intent intent = new Intent(this, FoodListActivity.class);
+        startActivity(intent);
+    }
+
+    public void OnSleepButtonClicked(View view) {
+        Intent intent = new Intent(this, SleepListActivity.class);
+        startActivity(intent);
     }
 }
